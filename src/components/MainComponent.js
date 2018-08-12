3fb912c3e64b858f303d5c162e101587e5332c78
Pavel <pniachai@gmail.com>
2018-08-12 21:38:40 +0200

    Git routing

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import  Menu from './MenuComponent';
-import  DishDetail from './DishDetailComponent' ;
+import Home from './HomeComponent';
+import Menu from './MenuComponent';
+import DishDetail from './DishDetailComponent' ;
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes.js';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -15,17 +17,21 @@ class Main extends Component {
         };
     }
 
-    onDishSelect(dishId){
-        this.setState({ selectedDishId: dishId });
-    }
-
-
   render() {
+    const HomePage = () => {
+      return(
+        <Home/>
+      );
+    }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} onClick ={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDishId)[0]} />
+          <Switch>
+            <Route path="/home" component ={HomePage}/>
+            <Route exact path="/menu" 
+              component = {() => <Menu dishes={this.state.dishes}/>} />
+             <Redirect to="/home"/>
+          </Switch>
         <Footer />
       </div>
     );
